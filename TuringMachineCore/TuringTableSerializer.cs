227187806b3c69a807340b88326c20c7db2ef8a1
Tom Richardson <tom.richardson@createitg.com>
2014-05-10 22:14:01 +0100

    Table serializer

---
+++ TuringMachineCore/TuringTableSerializer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuringMachineCore
+{
+    public class TuringTableSerializer
+    {
+        private string MachinePath;
+
+        public TuringTableSerializer()
+        {
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string MachinePath = Path.Combine(appDataPath, "TuringMachines");
+
+            if (!Directory.Exists(MachinePath))
+            {
+                Directory.CreateDirectory(MachinePath);
+            }
+        }
+
+        public void SaveTable(TuringTable table, string name)
+        {
+            string fullPath = Path.Combine(MachinePath, name + ".trg");
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(fullPath, FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, table);
+            stream.Close();
+        }
+        
+        public TuringTable LoadTable(string name)
+        {
+            string fullPath = Path.Combine(MachinePath, name + ".trg");
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(fullPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            TuringTable table = (TuringTable)formatter.Deserialize(stream);
+            stream.Close();
+            return table;
+        }
+        public IEnumerable<String> ListTables()
+        {
+            string[] tableFiles = Directory.GetFiles(MachinePath, "*.trg");
+            return tableFiles.Select(f => Path.GetFileNameWithoutExtension(f));
+        }
+    }
+}
